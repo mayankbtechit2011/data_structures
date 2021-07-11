cf277193f9cf746763107944bd3553a568ad659e
maysingh <mayank.singh@thalesgroup.com>
2021-07-11 20:44:36 +0530

    Defanging an IP Address program

---
+++ src/DefanginganIPAddress.java
@@ -0,0 +1,30 @@
+package com.java.july;
+
+/*Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+ 
+
+Example 1:
+
+Input: address = "1.1.1.1"
+Output: "1[.]1[.]1[.]1"
+Example 2:
+
+Input: address = "255.100.50.0"
+Output: "255[.]100[.]50[.]0"
+ */
+
+public class DefanginganIPAddress {
+
+	public static void main(String[] args) {
+		String address = "1.1.1.1";
+//		String address = "255.100.50.0";
+		System.out.println(defangIPaddr(address));
+	}
+
+	public static String defangIPaddr(String address) {
+		return address.replaceAll("\\.", "[.]");
+	}
+}
